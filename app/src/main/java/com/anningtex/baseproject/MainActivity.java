d9246c94e0d88e746bc8da37845fe1931d992b17
韩君帅 <1433452759@qq.com>
2020-06-19 17:52:59 +0800

    init

--- app/src/main/java/com/anningtex/baseproject/MainActivity.java
+++ app/src/main/java/com/anningtex/baseproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.anningtex.baseproject;
 
 import com.anningtex.baseproject.base.AbsBaseActivity;
+import com.anningtex.baseproject.base.BaseContract;
+import com.anningtex.baseproject.base.BasePresenter;
 
 /**
  * @Description: des
@@ -8,7 +10,7 @@ import com.anningtex.baseproject.base.AbsBaseActivity;
  * @Author: alvis
  * @CreateDate:2020-6-19 16:48:52
  */
-public class MainActivity extends AbsBaseActivity {
+public class MainActivity extends AbsBaseActivity<BasePresenter> implements BaseContract.View {
 
     @Override
     protected int getLayoutResId() {
