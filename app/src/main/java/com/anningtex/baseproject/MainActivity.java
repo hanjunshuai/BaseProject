8128088663164e3494e04de4c082044b15b603f8
韩君帅 <1433452759@qq.com>
2020-06-20 15:19:09 +0800

    init

--- app/src/main/java/com/anningtex/baseproject/MainActivity.java
+++ app/src/main/java/com/anningtex/baseproject/MainActivity.java
@@ -4,12 +4,6 @@ import com.anningtex.baselibrary.base.AbsBaseActivity;
 import com.anningtex.baselibrary.base.BaseContract;
 import com.anningtex.baselibrary.base.BasePresenter;
 
-/**
- * @Description: des
- * @ClassName: MainActivity
- * @Author: alvis
- * @CreateDate:2020-6-19 16:48:52
- */
 public class MainActivity extends AbsBaseActivity<BasePresenter> implements BaseContract.View {
 
     @Override
